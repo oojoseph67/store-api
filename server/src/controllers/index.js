d3676f991760c039b5bb170232406012cbde29ed
Joseph Okolo <oojoseph67@gmail.com>
2024-02-04 22:46:03 +0100

    made the endpoints dynamic

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,12 +2,80 @@ const Product = require("../models/index.js");
 
 const getAllProductsStatic = async (req, res) => {
   // throw new Error(`testing async errors`);
-  const products = await Product.find({ rating: 5 });
-  res.status(200).json({ nbHits: products.length, products });
+  const products = await Product.find({});
+  const nbHits = products.length;
+
+  res.status(200).json({ nbHits, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: `product testing route` });
+  try {
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    let queryObject = {};
+
+    if (featured) {
+      queryObject.featured = featured === "true" ? true : false;
+    }
+    if (company) {
+      queryObject.company = company;
+    }
+    if (name) {
+      // this format supports the search of few know words for the query
+      queryObject.name = { $regex: name, $options: "i" };
+    }
+    if (numericFilters) {
+      const operatorMap = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte",
+      };
+      const reqEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericfilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}`
+      );
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach((item) => {
+          const [field, operator, value] = item.split('-')
+          if(options.includes(field)){
+            queryObject[field] = {[operator]: Number(value)}
+          }
+        })
+      
+    }
+
+    console.log(queryObject);
+
+    // const product = await Product.find({ company: company });
+    let result = Product.find(queryObject);
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result = result.sort(sortList);
+    } else {
+      result = result.sort("createdAt");
+    }
+
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const product = await result;
+    const nbHits = product.length;
+
+    res.status(200).json({ nbHits, product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 };
 
 module.exports = {
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -11,7 +11,7 @@ const productSchema = new mongoose.Schema({
   },
   featured: {
     type: Boolean,
-    default: false,
+    default: true,
   },
   rating: {
     type: Number,
